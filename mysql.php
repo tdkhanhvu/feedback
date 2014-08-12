6036a22e1ff0a6873b645cc6c6db674d1d6b9006
QP <qp@powerme.com.sg>
2014-08-12 20:58:38 +0800

    added gitignore

--- mysql.php
+++ mysql.php
@@ -1,8 +1,12 @@
 <?php 
-$link = mysql_connect('toibalocom.ipagemysql.com', 'toifeedback', 'toifeedback'); 
-if (!$link) { 
-    die('Could not connect: ' . mysql_error()); 
-} 
-echo 'Connected successfully'; 
-// mysql_select_db(toifeedback); 
+	$link = null;
+
+	// 
+	$link = mysql_connect('toibalocom.ipagemysql.com', 'toifeedback', 'toifeedback');
+
+	if (!$link) { 
+	    die('Could not connect: ' . mysql_error()); 
+	} 
+	echo 'Connected successfully'; 
+	// mysql_select_db(toifeedback); 
 ?>
