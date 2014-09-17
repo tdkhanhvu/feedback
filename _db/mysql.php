7ff4aaed054d7e55aa4bcc1af126ab49eee0e0f0
QP <qp@powerme.com.sg>
2014-09-17 23:53:15 +0800

    add charset

--- _db/mysql.php
+++ _db/mysql.php
@@ -74,7 +74,7 @@ class MySQL {
 
 	// Construction
 	public function __construct() {
-		$this->dbh = new PDO('mysql:host=localhost;dbname=feedback;charset=utf8"', 'root', '');
+		$this->dbh = new PDO('mysql:host=localhost;dbname=feedback;charset=utf8', 'root', '');
 		//$this->dbh = new PDO('mysql:host=toibalocom.ipagemysql.com;dbname=feedback', 'tdkhanhvu', 'F%pks58F');
 	}
 
